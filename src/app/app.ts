dfdb016f8e61be8e3e8784095c6f4c7a992e2145
Darwin Bayas <tidomar@gmail.com>
2026-01-28 11:32:25 -0500

    separando en componentes la plantilla

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Cabecera } from "./cabecera/cabecera";
+import { Formulario } from "./formulario/formulario";
+import { Ingreso } from "./ingreso/ingreso";
+import { Egreso } from "./egreso/egreso";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Cabecera, Formulario, Ingreso, Egreso],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
